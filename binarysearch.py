cff285bb7d3d94ffc5d6818b8396d7047f02d0ce
Ananthraj <ananth@Ananthrajs-MacBook-Pro.local>
2025-12-30 12:58:14 +0530

    added bianry search program

---
+++ binarysearch.py
@@ -0,0 +1,53 @@
+
+import random
+import time
+
+
+def naive_search(l, target):
+    for i in range(len(l)):
+        if l[i] == target:
+            return i
+    return -1
+
+
+# binaryseacrh divide and conquer
+def bianrysaerch(l, target, low=None, high=None):
+    if low is None:
+        low = 0
+    if high is None:
+        high = len(l)-1
+
+    if high < low:
+        return -1
+    # example l = [1,3,5,10,12])
+    midpoint = (low+high)//2
+    if l[midpoint] == target:
+        return midpoint
+    elif target < l[midpoint]:
+        return bianrysaerch(l, target, low, midpoint-1)
+    else:
+        return bianrysaerch(l, target, midpoint+1, high)
+
+
+if __name__ == '__main__':
+    l = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21]
+    target = 7
+    print(naive_search(l, target))
+    print(bianrysaerch(l, target))
+    length = 10000
+    sorted_list = set()
+    while len(sorted_list) < length:
+        sorted_list.add(random.randint(-3*length, 3*length))
+    sorted_list = sorted(list(sorted_list))
+
+    start = time.time()
+    for target in sorted_list:
+        naive_search(sorted_list, target)
+    end = time.time()
+    print("naive saerch time:", (end-start)/length, "seconds")
+
+    start = time.time()
+    for target in sorted_list:
+        bianrysaerch(sorted_list, target)
+    end = time.time()
+    print("binary search time:", (end-start)/length, "seconds")
